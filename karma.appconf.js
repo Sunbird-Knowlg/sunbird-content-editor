3eedaa6c104472d1eaa643a4528756a82025c9b4
sunil <sunils@ilimi.in>
2016-11-08 15:20:27 +0530

    #7 added code coverage reporter and updated the gitignore

--- karma.appconf.js
+++ karma.appconf.js
@@ -46,13 +46,15 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'app/scripts/**/*.js': ['coverage']
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
@@ -85,6 +87,13 @@ module.exports = function(config) {
 
         // Concurrency level
         // how many browser should be started simultaneous
-        concurrency: Infinity
+        concurrency: Infinity,
+
+        coverageReporter: {
+            reporters: [
+                { type: 'html', dir: 'coverage/' },                                
+                { type: 'text-summary' }
+            ],
+        }
     })
 }
