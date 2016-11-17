6e3d4e131cb55af9ffa2b1b88fd6df58a830b415
sunil <sunils@ilimi.in>
2016-11-17 22:56:46 +0530

    fix: create blank stage when contentId is null
    changes:
    - added code create stage when contentId is null
    - uncommented getContent call
    - added scope variable contentId for enable/disable save button
    contributes: #39

--- app/scripts/angular/controller/main.js
+++ app/scripts/angular/controller/main.js
@@ -4,10 +4,10 @@
 'use strict';
 
 angular.module('editorApp', []);
-angular.module('editorApp').controller('MainCtrl', ['$scope', '$timeout', '$http','$location', '$q','$window',
+angular.module('editorApp').controller('MainCtrl', ['$scope', '$timeout', '$http', '$location', '$q', '$window',
     function($scope, $timeout, $http, $location, $q, $window) {
         EkstepEditorAPI.globalContext.contentId = $location.search().contentId;
-
+        $scope.contentId = EkstepEditorAPI.globalContext.contentId;
         $scope.safeApply = function(fn) {
             var phase = this.$root.$$phase;
             if (phase == '$apply' || phase == '$digest') {
@@ -29,33 +29,38 @@ angular.module('editorApp').controller('MainCtrl', ['$scope', '$timeout', '$http
         };
 
         $scope.previewContent = function() {
-            EkstepEditor.eventManager.dispatchEvent("atpreview:show", {contentBody: EkstepEditor.stageManager.toECML()});
+            EkstepEditor.eventManager.dispatchEvent("atpreview:show", { contentBody: EkstepEditor.stageManager.toECML() });
             /*$http.post('ecml', {data: EkstepEditor.stageManager.toECML()}).then(function(resp) {
                 console.info('ECML', resp.data);
             });*/
-        }
+        };
 
-        $scope.saveContent = function() {
+        if (!EkstepEditorAPI.globalContext.contentId) {
+            EkstepEditor.eventManager.dispatchEvent('stage:create', {});
+        };
+
+        $scope.saveContent = function(){
             var contentBody = EkstepEditor.stageManager.toECML();
             EkstepEditor.contentService.saveContent(EkstepEditorAPI.globalContext.contentId, contentBody, function(err, resp) {
-                //TODO: call popup service to show success message
+                if (resp) {
+                    alert('Content saved successfully');
+                }
             });
         }
-        
-        /*EkstepEditor.contentService.getContent(EkstepEditorAPI.globalContext.contentId, function(err, contentBody) {   
+
+        EkstepEditor.contentService.getContent(EkstepEditorAPI.globalContext.contentId, function(err, contentBody) {
             if (err) {
                 console.error('Unable to get content');
-            } 
+            }
             if(_.isUndefined(contentBody)) {
                 EkstepEditor.eventManager.dispatchEvent('stage:create', {});
                 EkstepEditorAPI.refreshStages();
             } else {
-                EkstepEditor.stageManager.fromECML(contentBody);                
-            }          
+                EkstepEditor.stageManager.fromECML(contentBody);
+            }
             EkstepEditor.stageManager.registerEvents();
-        });*/
-        EkstepEditor.stageManager.registerEvents();
-        EkstepEditor.eventManager.dispatchEvent('stage:create', {});
+        });
+
         /*
         $http.get('test.ecml').then(function(response) {
             var x2js = new X2JS({attributePrefix: 'none'});
