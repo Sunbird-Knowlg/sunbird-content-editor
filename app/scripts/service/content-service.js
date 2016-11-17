43b8187fcc555b5e6e5aecebe1f14941767105a7
sunil <sunils@ilimi.in>
2016-11-17 12:24:29 +0530

    #17 service code refactor
    Changes:
    - ContentService: removed promise based implementation to callback.
    - ContentService: contentMeta holds metadata of content (along with versionKey).
    - popupservice: removed promise based implementation to callback.

--- app/scripts/service/content-service.js
+++ app/scripts/service/content-service.js
@@ -2,18 +2,18 @@ EkstepEditor.contentService = new(EkstepEditor.iService.extend({
     serviceURL: EkstepEditor.config.baseURL + '/api/learning/',
     content: {},
     initService: function() {},
-    setContentMeta: function(id, versionKey) {
+    setContentMeta: function(id, contentMeta) {
         if (id) {
             this.content[id] = _.isUndefined(this.content[id]) ? {} : this.content[id];
-            this.content[id].versionKey = _.isUndefined(versionKey) ? this.content[id].versionKey : versionKey;
+            this.content[id].contentMeta = _.isUndefined(contentMeta) ? this.content[id].contentMeta : contentMeta;
         }
     },
     getContentMeta: function(id) {
         return this.content[id];
     },
     saveContent: function(contentId, body, callback) {
-        var instance = this,                        
-            versionKey = instance.content[contentId].versionKey;
+        var instance = this,
+            versionKey = instance.content[contentId].contentMeta.versionKey;
 
         if (contentId && versionKey && body) {
             var requestObj = {
@@ -24,38 +24,29 @@ EkstepEditor.contentService = new(EkstepEditor.iService.extend({
                     }
                 }
             };
-
             console.log("save content: ", requestObj);
 
-            instance.http.patch(this.serviceURL + 'v2/content/' + contentId, requestObj)
-                .then(function success(response) {
-                    console.log("success response", response);
-                    instance.setContentMeta(contentId, response.data.result.versionKey);
-                    callback(null, response);                    
-                }, function error(response) {
-                    callback(response, null);
-                });
+            instance.http.patch(this.serviceURL + 'v2/content/' + contentId, requestObj, {}, serviceCallback);
+
+            function serviceCallback(err, res) {                
+                if (res) {
+                    instance.content[contentId].contentMeta.versionKey=res.data.result.versionKey;                    
+                }
+                callback(err, res);
+            }
         }
     },
     getContent: function(contentId, callback) {
-        var instance = this;
-        if (contentId) {
-            var metaDataFields = "body,editorState,templateId,languageCode,template,gradeLevel,status,concepts,versionKey";
-            instance.http.get(this.serviceURL + 'v2/content/' + contentId + "?fields=" + metaDataFields)
-                .then(function success(response) {
-                    instance.setContentMeta(contentId, response.data.result.content.versionKey);
-                    callback(null, response.data.result.content.body);
-                }, function error(response) {
-                    callback(response, null);
-                });
+        var instance = this;        
+        if (contentId) {            
+            instance.http.get(this.serviceURL + 'v2/content/' + contentId, {}, serviceCallback);
+            function serviceCallback(err, res) {                
+                if (res) {
+                    instance.setContentMeta(contentId, res.data.result.content);                    
+                }
+                callback(err, res.data.result.content.body);
+            };
+                
         }
-    },
-    getContentMetadata: function(contentId, callback) {
-        this.http.get(this.serviceURL + 'v2/content/' + contentId)
-            .then(function success(response) {
-                callback(null, response);
-            }, function error(response) {
-                callback(response, null);
-            });
     }
 }));
--- app/scripts/service/popup-service.js
+++ app/scripts/service/popup-service.js
@@ -1,13 +1,14 @@
-EkstepEditor.popupService = new (EkstepEditor.iService.extend({
+EkstepEditor.popupService = new(EkstepEditor.iService.extend({
     initService: function() {
         this.$uibModal = angular.injector(['ng', 'ui.bootstrap']).get('$uibModal');
     },
-    open: function(options) {
+    open: function(options, cb) {
         if (options) {
+            if (!cb) cb = function() {};            
             options.template += _.isUndefined(options.modal_header) ? "" : '<div class=\"modal-header\">' + options.modal_header + '<\/div>';
             options.template += _.isUndefined(options.modal_content) ? "" : '<div class=\"modal-body\">' + options.modal_content + '<\/div>';
             options.template += _.isUndefined(options.modal_footer) ? "" : '<div class=\"modal-footer\">' + options.modal_footer + '<\/div>';
-            return this.$uibModal.open(options);
+            this.$uibModal.open(options).rendered.then(cb(options.data));
         }
     }
 }));
