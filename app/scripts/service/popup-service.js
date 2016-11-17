da57ca1b5696bc8f15fb0782191fdf737fd3792a
sunil <sunils@ilimi.in>
2016-11-17 14:27:43 +0530

    feat: Global Context object in API
    Changes:
    - setting contentId inside Global Context
    - popupService reverting back to promise implementation
    
    Contributions: #17

--- app/scripts/service/popup-service.js
+++ app/scripts/service/popup-service.js
@@ -2,13 +2,12 @@ EkstepEditor.popupService = new(EkstepEditor.iService.extend({
     initService: function() {
         this.$uibModal = angular.injector(['ng', 'ui.bootstrap']).get('$uibModal');
     },
-    open: function(options, cb) {
+    open: function(options) {
         if (options) {
-            if (!cb) cb = function() {};            
-            options.template += _.isUndefined(options.modal_header) ? "" : '<div class=\"modal-header\">' + options.modal_header + '<\/div>';
+            options.template = _.isUndefined(options.modal_header) ? "" : '<div class=\"modal-header\">' + options.modal_header + '<\/div>';
             options.template += _.isUndefined(options.modal_content) ? "" : '<div class=\"modal-body\">' + options.modal_content + '<\/div>';
             options.template += _.isUndefined(options.modal_footer) ? "" : '<div class=\"modal-footer\">' + options.modal_footer + '<\/div>';
-            this.$uibModal.open(options).rendered.then(cb(options.data));
+            return this.$uibModal.open(options);
         }
     }
 }));
