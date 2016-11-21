8a18e5c4bce0c5d5fa4b4164e85d99b955c75c82
sunil <sunils@ilimi.in>
2016-11-21 11:51:58 +0530

    fix: popupservice
    Changes:
    - added default conditions for mandatory properties.
    
    contributes: #17

--- app/scripts/service/popup-service.js
+++ app/scripts/service/popup-service.js
@@ -5,6 +5,9 @@ EkstepEditor.popupService = new(EkstepEditor.iService.extend({
     },
     open: function(options, callback) {
         if (options && options.template) {
+            options.controller = options.controller ? options.controller : 'popupController';
+            options.controllerAs = options.controllerAs ? options.controllerAs : '$ctrl';
+            options.resolve = options.resolve ? options.resolve : { data: {} };
             var popuptemplateHolder = "<script type=\"text\/ng-template\" id=\"popuptemplate\">" + options.template + "<\/script>";
             EkstepEditor.jQuery('#popuptemplate').remove();
             EkstepEditor.jQuery('#editorContainer').append(popuptemplateHolder);
