bb98e05878a9254c62312781a2f2459daa11b867
Harishkumar Gangula <harishg@ilimi.in>
2016-11-23 15:14:57 +0530

    fix: updated karma config -excluded coverage, libs , test folders from plugins contributes:https://github.com/ekstep/Content-Editor/issues/7

--- karma.pluginconf.js
+++ karma.pluginconf.js
@@ -52,13 +52,13 @@ module.exports = function(config) {
 
 
         // list of files to exclude
-        exclude: [],
+        exclude: ['plugins/**/test/**/*','plugins/**/libs/**','plugins/coverage/**'],
 
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'plugins/**/!(libs)/*.js': 'coverage',
+            'plugins/**/*.js': ['coverage']
         },
 
 
