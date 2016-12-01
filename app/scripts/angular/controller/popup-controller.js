788d4a063dca04e61c58f4066a1a2dc53247b650
Kartheek Palla <kartheek@Kartheeks-MacBook-Pro.local>
2016-12-01 18:18:04 +0530

    Removed Bootstrap Ui Model

--- app/scripts/angular/controller/popup-controller.js
+++ app/scripts/angular/controller/popup-controller.js
@@ -1,9 +1,25 @@
-angular.module('editorApp').controller('popupController', ['$scope','$uibModalInstance', 'data', '$sce', function($scope, $uibModalInstance, data, $sce) {
+angular.module('editorApp').controller('popupController', ['$scope', '$sce', '$compile', '$document', function($scope, $sce, $compile, $document) {
     var $ctrl = this,
-        args = Array.prototype.slice.call(arguments);
+        args;
 
-    args.unshift($ctrl);
-    EkstepEditorAPI.addEventListener('popupservice:fire', function(events, callback) {
-        callback.apply($ctrl, args);
-    }, $ctrl);
+    args = [$ctrl, $scope];
+    EkstepEditorAPI.dispatchEvent('popupModal:show', semanticModal);
+
+    function semanticModal(data) {
+        $compile($document.find('.ui.modal'))($scope);
+        $('.ui.modal').modal({
+            onShow: function() {
+                args.push(data);
+                EkstepEditorAPI.dispatchEvent('popupservice:controller:load', {
+                    controller: $ctrl,
+                    args: args
+                });
+            },
+            onHide: function() {
+
+            }
+        }).modal('show');
+    };
+
+
 }]);
--- app/scripts/service/popup-service.js
+++ app/scripts/service/popup-service.js
@@ -1,19 +1,22 @@
 EkstepEditor.popupService = new(EkstepEditor.iService.extend({
-    $uibModal: undefined,
-    initService: function(instance) {
-        this.$uibModal = instance;
+    callback: undefined,
+    modal: undefined,
+    initService: function() {
+        var thisObj = this;
+        EkstepEditorAPI.addEventListener('popupModal:show', function(event, callback) {
+            thisObj.modal = callback;
+        }, this);
+
+        EkstepEditorAPI.addEventListener('popupservice:controller:load', function(event, data) {
+            thisObj.callback.apply(data.controller, data.args);
+        }, this);
     },
     open: function(options, callback) {
         if (options && options.template) {
-            options.controller = options.controller ? options.controller : 'popupController';
-            options.controllerAs = options.controllerAs ? options.controllerAs : '$ctrl';
-            options.resolve = options.resolve ? options.resolve : { data: {} };
-            var popuptemplateHolder = "<script type=\"text\/ng-template\" id=\"popuptemplate\">" + options.template + "<\/script>";
-            EkstepEditor.jQuery('#popuptemplate').remove();
-            EkstepEditor.jQuery('#editorContainer').append(popuptemplateHolder);
-            this.$uibModal.open(options).rendered.then(function() {
-                EkstepEditorAPI.dispatchEvent('popupservice:fire', callback);
-            });
+            this.callback = callback;
+            $(".ui.modal").remove();
+            EkstepEditor.jQuery('#popuptemplate').append(options.template);
+            this.modal(options.data);
         }
     }
 }));
