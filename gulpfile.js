87ff0e8e8d50809dd669ee3169146844311c9501
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-12-09 02:11:38 +0530

    refactor: Semantic UX
    Update columns from '16' to '12' in site.variables
    
    contributes: #48

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
  gulp.task('setup', function() {
      gulp.src('semantic/dist', { read: false }).pipe(clean())
      gulp.src(['app/config/theme.config']).pipe(gulp.dest('semantic/src/'))
+     gulp.src(['app/config/site.variables']).pipe(gulp.dest('semantic/src/site/globals/'))
      gulp.src('semantic/gulpfile.js')
          .pipe(chug({tasks: ['build']}, function() {
              gulp.src(['semantic/dist/semantic.min.css']).pipe(gulp.dest('app/styles/'));
