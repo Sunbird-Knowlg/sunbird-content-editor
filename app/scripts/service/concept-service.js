5aa21ca1ca65ee1eb7c0d37cb626da98bd4df534
Chetan Sachdev <cksachdev@gmail.com>
2016-12-12 16:24:44 +0530

    fix: Added concept service for Concept Browser plugin
    contributes: https://github.com/ekstep/Content-Plugins/issues/48

---
+++ app/scripts/service/concept-service.js
@@ -0,0 +1,27 @@
+EkstepEditor.conceptService = new(EkstepEditor.iService.extend({
+    learningURL: EkstepEditor.config.baseURL + '/api/learning/',
+    searchURL: EkstepEditor.config.baseURL + '/api/search/',
+    requestHeaders: {
+        "headers": {
+            "content-type": "application/json",
+            "user-id": "content-editor"
+        }
+    },
+    templateHeaders: {
+        "headers": {
+            "content-type": "application/json",
+            "user-id": "content-editor"
+        }
+    },
+    initService: function() {},
+    getConceptsTree: function(callback) {
+        var data = {"request": { "filters":{ "objectType": ["Dimension","Domain","Concept"]}, "limit": 500}};
+        this.postFromService(this.searchURL + 'v2/search',data, this.requestHeaders, callback);
+    },
+    postFromService: function(url, data, headers, callback) {
+        var instance = this;
+        instance.http.post(url, JSON.stringify(data), headers, function(err, res) {
+            callback(err, res)
+        });
+    }
+}));
