e5b49b3f4320f6aaa446b2c3489011eb25a2cea2
sunil <sunils@ilimi.in>
2016-12-20 12:55:34 +0530

    refactor: popup service:
    changes:
    - removed unused dependencies in popup controller
    
    contributes: #77

--- app/scripts/angular/controller/popup-controller.js
+++ app/scripts/angular/controller/popup-controller.js
@@ -1,4 +1,4 @@
-angular.module('editorApp').controller('popupController', ['$scope', '$compile', '$document', '$injector', 'ngDialog', '$ocLazyLoad', function($scope, $compile, $document, $injector, ngDialog, $ocLazyLoad) {
+angular.module('editorApp').controller('popupController', ['ngDialog', '$ocLazyLoad', function(ngDialog, $ocLazyLoad) {
     function loadNgModules(templatePath, controllerPath) {
         $ocLazyLoad.load([
             { type: 'html', path: templatePath },
