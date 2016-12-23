f734979d3ac9af3b3948623579cf89c07f76ae32
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-12-23 18:00:56 +0530

    refactor: Delete unecessary config files #21

--- app/config/devAPIurl.js
+++
@@ -1,8 +0,0 @@
-EkstepEditor.serviceURL = {
-    "baseURL": "https://dev.ekstep.in",
-    "learningServiceReverseProxyUrl": "/api/learning/",
-    "assetReverseProxyUrl": "/assets/public/",
-    "previewReverseProxyUrl": "/assets/public/preview/dev/preview.html?webview=true",
-    "searchServiceBaseUrl": "https://dev.ekstep.in/api/search/",
-    "aws_s3": "https://ekstep-public.s3-ap-southeast-1.amazonaws.com/"
-};
