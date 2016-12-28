28541fd2bae1460cd82ccd9faf190f58a6c37102
sunil <sunils@ilimi.in>
2016-12-28 11:31:55 +0530

    refactor: image migration task
    changes:
    -it should not migrate the image. currently it is used to remove scribble eraser image
    
    contributes: #51

--- app/scripts/migration/imagemigration-task.js
+++ app/scripts/migration/imagemigration-task.js
@@ -4,20 +4,13 @@ EkstepEditor.imagemigration_task = new(Class.extend({
     init: function() {
         console.log('image migration initialized');
     },
-    id: 'org.ekstep.image',
     migrate: function(contentbody) {
-    	var deferred = EkstepEditor.$q.defer();
-        var instance = this;
-        _.forEach(contentbody.theme.stage, function(stage, index) {
-            if (stage.image && stage.image.length) stage[instance.id] = stage.image;            
-            if (stage.image) delete stage.image;
-            if (contentbody.theme.stage.length === index + 1) deferred.resolve(contentbody);
-        });
+        var deferred = EkstepEditor.$q.defer();
+        deferred.resolve(contentbody);
         return deferred.promise;
     },
     removeImage: function(stage, id) {
-        var image = _.isUndefined(stage.image) ? this.id : 'image';
-        _.remove(stage[image], function(image) {
+        _.remove(stage.image, function(image) {
             return image.asset === id;
         });
     }
