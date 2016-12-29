f634bf70f31f33cfe4273fbc25c67ca7e85d5c66
sunil <sunils@ilimi.in>
2016-12-29 15:28:31 +0530

    feat: read along migration task
    changes:
    - attribute ’isReadAlongAutoPlay’ to be converted to ‘autoplay’.
    
    contributes: #51

--- app/scripts/migration/1_migration-task.js
+++ app/scripts/migration/1_migration-task.js
@@ -6,7 +6,7 @@ EkstepEditor.migration = new(Class.extend({
         EkstepEditor.$q = angular.injector(['ng']).get('$q'); //promise
         EkstepEditorAPI.addEventListener('migrationTask:start', this.execute, this);
     },
-    tasks: ['basestage_task', 'orderstage_task', 'scribblemigration_task', 'imagemigration_task'],
+    tasks: ['basestage_task', 'orderstage_task', 'scribblemigration_task', 'imagemigration_task','readalongmigration_task'],
     execute: function(event, contentbody) {
         var instance = this;
         if (!this.versionCompatible(contentbody.theme.version || contentbody.theme.ver)) {
@@ -14,6 +14,7 @@ EkstepEditor.migration = new(Class.extend({
                 .then(function(content) {return EkstepEditor.migration[instance.tasks[1]].migrate(content)})
                 .then(function(content) {return EkstepEditor.migration[instance.tasks[2]].migrate(content)})
                 .then(function(content) {return EkstepEditor.migration[instance.tasks[3]].migrate(content)})                
+                .then(function(content) {return EkstepEditor.migration[instance.tasks[4]].migrate(content)})                
                 .then(function(content) {
                     console.info('Migration task completed!');
                     instance.setNewVersion(content);
---
+++ app/scripts/migration/readalongmigration-task.js
@@ -0,0 +1,20 @@
+'use strict';
+
+EkstepEditor.migration.readalongmigration_task = new(Class.extend({
+    init: function() {
+        console.log('read along migration task initialized');
+    },
+    // check 'isReadAlongAutoPlay' attribute in htext and convert to 'autoplay'
+    migrate: function(contentbody) {
+    		console.log('migrating readalong');
+        var deferred = EkstepEditor.$q.defer();
+        _.forEach(contentbody.theme.stage, function(stage) {
+            if (stage.htext) {
+                _.forEach(stage.htext, function(htext) {
+                    if(!_.isUndefined(htext.isReadAlongAutoPlay)) (htext.autoplay = htext.isReadAlongAutoPlay) && (delete htext.isReadAlongAutoPlay) && (deferred.resolve(contentbody));                    	
+                });
+            }
+        });
+        return deferred.promise;
+    }
+}));
