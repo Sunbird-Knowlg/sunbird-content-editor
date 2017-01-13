8cb297918847ed7f3609f436be80c8af121ec854
sunil <sunils@ilimi.in>
2017-01-13 12:19:07 +0530

    refactor: migration task
    changes:
    - read along migration: if htext not an array, convert to array.
    
    contributes: #89

--- app/scripts/migration/readalongmigration-task.js
+++ app/scripts/migration/readalongmigration-task.js
@@ -9,9 +9,13 @@ EkstepEditor.migration.readalongmigration_task = new(Class.extend({
     		console.log('migrating readalong');
         var deferred = EkstepEditor.$q.defer();
         _.forEach(contentbody.theme.stage, function(stage, index) {
-            if (stage.htext) {
+            if (stage.htext && (!_.isArray(stage.htext))) stage.htext = [stage.htext];
+            if (stage.htext && stage.htext.length) {
                 _.forEach(stage.htext, function(htext) {
-                    if(!_.isUndefined(htext.isReadAlongAutoPlay)) (htext.autoplay = htext.isReadAlongAutoPlay) && (delete htext.isReadAlongAutoPlay);
+                    if(!_.isUndefined(htext.isReadAlongAutoPlay)) {
+                        htext.autoplay = htext.isReadAlongAutoPlay;
+                        delete htext.isReadAlongAutoPlay;
+                    }
                 });
             }
             if(contentbody.theme.stage.length === index + 1)  deferred.resolve(contentbody);                    	
