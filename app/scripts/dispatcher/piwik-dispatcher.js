a34a512d0c6e4700f58a73588581b14023de8da8
sunil <sunils@ilimi.in>
2017-02-16 17:27:31 +0530

    Issue #136 update: telemetry
    changes:
    - piwik dispatcher implementation

--- app/scripts/dispatcher/piwik-dispatcher.js
+++ app/scripts/dispatcher/piwik-dispatcher.js
@@ -1,7 +1,21 @@
 EkstepEditor.piwikDispatcher = new(EkstepEditor.IDispatcher.extend({
-		type: "piwikDispatcher",
+    type: "piwikDispatcher",
+    piwikEndPoint: "https://devqa-analytics.ekstep.in/piwik.php",
+    idsite: 1,
     initDispatcher: function() {},
     dispatch: function(event) {
-        //TODO: Implementation pending
+        if (!event) return;
+
+        try {
+            event = (typeof event === "string") ? event : JSON.stringify(event);
+            EkstepEditorAPI.jQuery.post(this.piwikEndPoint + '?idsite=' + this.idsite + '&url=' + EkstepEditor.config.absURL+ location.pathname + '&e_c=ContentEditor&e_a=' + event + '&rec=1', function() {
+                    //console.log('piwik event dispatched');
+                })
+                .fail(function() {
+                    console.log("error: while piwik dispatch");
+                });
+        } catch (e) {
+            console.log('error: piwik event cannot be stringify', e);
+        }
     }
 }));
