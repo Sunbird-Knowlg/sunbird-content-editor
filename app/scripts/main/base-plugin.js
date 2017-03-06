93682eff83a7cac46561664cf73881a26fe77046
unknown <anshu.narayan@tarento.com>
2017-03-06 17:01:27 +0530

    Issue #235 fix : Rotation of Object

--- app/scripts/main/base-plugin.js
+++ app/scripts/main/base-plugin.js
@@ -72,17 +72,14 @@ EkstepEditor.basePlugin = Class.extend({
      * @memberof EkstepEditor.BasePlugin
      */
     postInit: function() {
-        var instance = this;
         this.registerFabricEvents();
         if (this.editorObj) { this.editorObj.set({ id: this.id }); this.editorObj.setVisible(true); }
         if (this.parent) this.parent.addChild(this);
         if (this.parent && this.parent.type !== 'stage') EkstepEditorAPI.dispatchEvent('object:modified', { id: this.id });
-        if(_.has(this.manifest.editor, 'behaviour')){
-            _.forOwn(this.manifest.editor.behaviour, function(value, key) {
-                if(key === "rotatable" && key){
-                    if (instance.editorObj) { instance.editorObj.hasRotatingPoint = true; }
-                }
-            });
+        if(_.has(this.manifest.editor, 'behaviour')) {
+            if(!_.isUndefined(this.manifest.editor.behaviour.rotatable) && (this.manifest.editor.behaviour.rotatable === true)) {
+                if (this.editorObj) { this.editorObj.hasRotatingPoint = true; }
+            }
         }
     },
 
