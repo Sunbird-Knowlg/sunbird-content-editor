fa342eff1539c4ed0370829c8fe7241c5849ad25
sunil <sunils@ilimi.in>
2017-03-17 20:22:07 +0530

    Issue #242 update: framework testing
    changes:
    - load core plugins from s3

--- app/test/scripts/framework.spec.js
+++ app/test/scripts/framework.spec.js
@@ -1,16 +1,32 @@
-describe("Framework test cases", function() {
-    EkstepEditor.config.plugins = {
-        "org.ekstep.test1": "1.0",
-        "org.ekstep.test2": "1.0",
-        "org.ekstep.test3": "1.0",
-        "org.ekstep.test4": "1.0",
-        "org.ekstep.test5": "1.0"
-    };    
-    EkstepEditor.publishedRepo.basePath = "base/app/test/data/published";
-    EkstepEditor.hostRepo.basePath = "base/app/test/data/hosted";
-    EkstepEditor.draftRepo.basePath = "base/app/test/data/draft";
-    EkstepEditor.hostRepo.connected = true;
+describe(" framework integration", function() {
+    beforeAll(function() {        
+        var corePlugins = {
+            "org.ekstep.stage": "1.0"
+        };
+        
+        // test plugins
+        EkstepEditor.config.plugins = {
+            "org.ekstep.test1": "1.0",
+            "org.ekstep.test2": "1.0",
+            "org.ekstep.test3": "1.0",
+            "org.ekstep.test4": "1.0",
+            "org.ekstep.test5": "1.0"
+        };
 
-    EkstepEditor.pluginManager.loadAllPlugins(EkstepEditor.config.plugins, function(){});
+        //load core plugins from s3
+        EkstepEditor.publishedRepo.basePath = "https://s3.ap-south-1.amazonaws.com/ekstep-public-dev/content-plugins";
+        _.forIn(corePlugins, function(value, key) {
+            EkstepEditor.pluginManager.loadPlugin(key, value);
+        });
+        
+        EkstepEditor.publishedRepo.basePath = "base/app/test/data/published";
+        EkstepEditor.hostRepo.basePath = "base/app/test/data/hosted";
+        EkstepEditor.draftRepo.basePath = "base/app/test/data/draft";
+        EkstepEditor.hostRepo.connected = true;
+    });
 
+    it("should register plugins with plugin manager", function() {
+        EkstepEditor.pluginManager.loadAllPlugins(EkstepEditor.config.plugins, function() {});
+        //assert plugin registry
+    });
 });
