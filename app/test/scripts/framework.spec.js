faff88feee3bebcb263bd2e8b5ac6c70ef7eed8a
Harishkumar Gangula <harishg@ilimi.in>
2017-03-17 17:07:28 +0530

    Issue #242 feat: framework initial commit

---
+++ app/test/scripts/framework.spec.js
@@ -0,0 +1,11 @@
+
+describe("Framework test cases", function() {
+    EkstepEditor.config.plugins = { "org.ekstep.test1": "1.0", 
+                                    "org.ekstep.test2": "1.0",
+                                    "org.ekstep.test3": "1.0",
+                                    "org.ekstep.test4": "1.0",
+                                    "org.ekstep.test5": "1.0"
+                                };
+
+});
+
