2b29475e8f96309245b6f82e8259616b1ab4e91b
sunil <sunils@ilimi.in>
2017-03-17 19:29:53 +0530

    Issue #242 update: load test plugins
    changes:
    - loading test plugins for framework test

--- app/test/scripts/framework.spec.js
+++ app/test/scripts/framework.spec.js
@@ -1,11 +1,16 @@
-
 describe("Framework test cases", function() {
-    EkstepEditor.config.plugins = { "org.ekstep.test1": "1.0", 
-                                    "org.ekstep.test2": "1.0",
-                                    "org.ekstep.test3": "1.0",
-                                    "org.ekstep.test4": "1.0",
-                                    "org.ekstep.test5": "1.0"
-                                };
+    EkstepEditor.config.plugins = {
+        "org.ekstep.test1": "1.0",
+        "org.ekstep.test2": "1.0",
+        "org.ekstep.test3": "1.0",
+        "org.ekstep.test4": "1.0",
+        "org.ekstep.test5": "1.0"
+    };    
+    EkstepEditor.publishedRepo.basePath = "base/app/test/data/published";
+    EkstepEditor.hostRepo.basePath = "base/app/test/data/hosted";
+    EkstepEditor.draftRepo.basePath = "base/app/test/data/draft";
+    EkstepEditor.hostRepo.connected = true;
+
+    EkstepEditor.pluginManager.loadAllPlugins(EkstepEditor.config.plugins, function(){});
 
 });
-
