20c7da90dd6434740333a8b437dec5c7a980de7a
Santhosh <santhosh@ilimi.in>
2017-03-25 23:14:46 +0530

    Issue #242 fix: framework test case issues

--- app/scripts/main/ekstep-editor.js
+++ app/scripts/main/ekstep-editor.js
@@ -1,6 +1,7 @@
 /**
  * @author Santhosh Vasabhaktula <santhosh@ilimi.in>
  */
+/* istanbul ignore next. Fabric extension - cannot be tested */
 window.fabric.Object.prototype.toObject = (function(toObject) {
     return function() {
         return window.fabric.util.object.extend(toObject.call(this), {
