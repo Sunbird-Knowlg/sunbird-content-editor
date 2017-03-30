cbb75eb5434a1980e2e14a0350b612eac65b5cd7
Santhosh <santhosh@ilimi.in>
2017-03-30 12:48:51 +0530

    Issue #268 fix: Add backward compatibility for iService

--- app/scripts/contenteditor/backward-compatibility.js
+++ app/scripts/contenteditor/backward-compatibility.js
@@ -3,7 +3,9 @@
  */
 window.EkstepEditorAPI = org.ekstep.contenteditor.api;
 window.EkstepEditor = org.ekstep.contenteditor;
+EkstepEditor.iService = org.ekstep.services.iService;
 
+/* Deprecated variables */
 EkstepEditorAPI.apislug = org.ekstep.contenteditor.config.apislug;
 EkstepEditorAPI.baseURL = org.ekstep.contenteditor.config.baseURL;
 EkstepEditorAPI.absURL = org.ekstep.contenteditor.config.absURL;
