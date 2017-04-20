f1ec9c4e2a86e1162773a4c0a46180681bd507a7
Chetan Sachdev <cksachdev@gmail.com>
2017-04-20 19:49:14 +0530

    Issue #297 fix: Karma configuration setup - revert back to PhantomJS

--- karmaconf.js
+++ karmaconf.js
@@ -94,7 +94,6 @@ module.exports = function(config) {
         ],
 
         plugins: [
-            'karma-chrome-launcher',
             'karma-jasmine',
             'karma-jasmine-matchers',
             'karma-coverage',
@@ -139,18 +138,10 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: [
-            "Chrome", "Chrome_without_security"
+            "PhantomJS"
         ],
-        // you can define custom flags 
-        customLaunchers: {
-          Chrome_without_security: {
-            base: 'Chrome',
-            flags: ['--disable-web-security']
-          }
-        },
 
         browserNoActivityTimeout: 100000,
-
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
