fa4b7696484980fb7fe151b881d7a8659fd9955c
sunil <sunils@ilimi.in>
2017-05-02 15:13:24 +0530

    Issue #294 refactor: plugin framework
    changes:
    - removed lodash method

--- app/scripts/framework/manager/resource-manager.js
+++ app/scripts/framework/manager/resource-manager.js
@@ -43,7 +43,7 @@ org.ekstep.pluginframework.resourceManager = new(Class.extend({
         });
 
         if (!repoFound) {
-            if (!_.isUndefined(position)) this.registeredRepos.splice(position, 0, repo)
+            if (typeof position === 'number') this.registeredRepos.splice(position, 0, repo)
             else this.registeredRepos.push(repo);
         } else {
             console.error(repo.id + ': Repo already registered!');
