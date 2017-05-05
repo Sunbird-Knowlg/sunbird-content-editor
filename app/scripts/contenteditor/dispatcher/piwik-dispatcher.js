d42cb5c06b993e692d3a202265805e0826a2556e
sunil <sunils@ilimi.in>
2017-05-05 17:39:41 +0530

    Issue #306 update: piwik
    changes:
    - event data sending inside request body.

--- app/scripts/contenteditor/dispatcher/piwik-dispatcher.js
+++ app/scripts/contenteditor/dispatcher/piwik-dispatcher.js
@@ -1,7 +1,7 @@
 org.ekstep.contenteditor.piwikDispatcher = new(org.ekstep.contenteditor.IDispatcher.extend({
     type: "piwikDispatcher",
     piwikEndPoint: function() {
-        return org.ekstep.contenteditor.config.baseURL + "/piwik/piwik.php";
+        return org.ekstep.contenteditor.config.baseURL + "/piwik";
     },
     idsite: 1,
     initDispatcher: function() {},
@@ -11,7 +11,7 @@ org.ekstep.contenteditor.piwikDispatcher = new(org.ekstep.contenteditor.IDispatc
         try {
             event = (typeof event === "string") ? event : JSON.stringify(event);
             /* istanbul ignore next. Cannot test jquery post */
-            org.ekstep.contenteditor.jQuery.post(this.piwikEndPoint() + '?idsite=' + this.idsite + '&url=' + org.ekstep.services.config.absURL + location.pathname + '&e_c=ContentEditor&e_a=' + event + '&rec=1', function() {
+            org.ekstep.contenteditor.jQuery.post(this.piwikEndPoint(), '?idsite=' + this.idsite + '&url=' + org.ekstep.services.config.absURL + location.pathname + '&e_c=ContentEditor&e_a=' + event + '&rec=1', function() {
             })
             .fail(function() {
                 console.log("error: while piwik dispatch");
