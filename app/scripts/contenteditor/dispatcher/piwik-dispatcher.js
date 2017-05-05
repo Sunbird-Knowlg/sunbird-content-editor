923f2b7c0568e3b55bb1e8ad7cadd446dab1bb03
sunil <sunils@ilimi.in>
2017-05-05 18:23:51 +0530

    Issue #306 update: piwik
    changes:
    - absURL changes

--- app/scripts/contenteditor/dispatcher/piwik-dispatcher.js
+++ app/scripts/contenteditor/dispatcher/piwik-dispatcher.js
@@ -11,7 +11,7 @@ org.ekstep.contenteditor.piwikDispatcher = new(org.ekstep.contenteditor.IDispatc
         try {
             event = (typeof event === "string") ? event : JSON.stringify(event);
             /* istanbul ignore next. Cannot test jquery post */
-            org.ekstep.contenteditor.jQuery.post(this.piwikEndPoint(), 'idsite=' + this.idsite + '&url=' + org.ekstep.services.config.absURL + location.pathname + '&e_c=ContentEditor&e_a=' + event + '&rec=1', function() {
+            org.ekstep.contenteditor.jQuery.post(this.piwikEndPoint(), 'idsite=' + this.idsite + '&url=' + org.ekstep.contenteditor.config.absURL + location.pathname + '&e_c=ContentEditor&e_a=' + event + '&rec=1', function() {
             })
             .fail(function() {
                 console.log("error: while piwik dispatch");
