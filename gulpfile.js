02bf661482937de2c066e1ada7205a9844c9b5c9
sunil <sunils@ilimi.in>
2017-06-23 08:43:26 +0530

    Issue CE-Core-Plugins#458 chore: gulp task added keyboardshortcut plugin

--- gulpfile.js
+++ gulpfile.js
@@ -322,7 +322,8 @@ var corePlugins = [
     "org.ekstep.review-1.0",
     "org.ekstep.video-1.0",
     "org.ekstep.editorstate-1.0",
-    "org.ekstep.ceheader-1.0"
+    "org.ekstep.ceheader-1.0",
+    "org.ekstep.keyboardshortcuts-1.0"
 ]
 
 gulp.task('minifyCorePlugins', function() {
