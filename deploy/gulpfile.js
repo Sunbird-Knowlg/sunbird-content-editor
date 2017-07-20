9d4e57ebb3b344ae795f77dfc72e935e04549fa6
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-20 11:33:42 +0530

    Issue #366 fix: injecting only script.min.js

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -10,14 +10,14 @@ var promise = require("any-promise");
 
 var cachebust = new CacheBuster();
 gulp.task('renameminifiedfiles', function() {
-    var js =  gulp.src('scripts/*.min.js').pipe(cachebust.resources()).pipe(gulp.dest('scripts/'));
+    var js =  gulp.src(['scripts/external.min.js', 'scripts/script.min.js']).pipe(cachebust.resources()).pipe(gulp.dest('scripts/'));
     var css = gulp.src('styles/*.min.css').pipe(cachebust.resources()).pipe(gulp.dest('styles/'));
     return mergeStream(js, css);
 });
 
 gulp.task('injectrenamedfiles', function() {
     var target = gulp.src('index.html');
-    var sources = gulp.src(['scripts/*.min.*.js', 'styles/*.min.*.css'], { read: false });
+    var sources = gulp.src(['scripts/external.min.*.js', 'scripts/script.min.*.js', 'styles/*.min.*.css'], { read: false });
     return target.pipe(inject(sources, { ignorePath: '/', addRootSlash: false })).pipe(gulp.dest('./'));
 });
 
