4a6ccdf7da456be0fbee70aae0e990724cb9a77e
sunil <sunils@ilimi.in>
2017-08-30 11:36:59 +0530

    Issue #217 chore: previewURL

--- app/scripts/contenteditor/ce-config.js
+++ app/scripts/contenteditor/ce-config.js
@@ -14,7 +14,7 @@ org.ekstep.contenteditor.config = {
     corePluginsPackaged: true,
     dispatcher: "local",
     localDispatcherEndpoint: "/app/telemetry",
-    previewURL: "/content/preview/preview.html"
+    previewURL: "preview/preview.html"
 }
 
 org.ekstep.contenteditor.extendedConfig = {
