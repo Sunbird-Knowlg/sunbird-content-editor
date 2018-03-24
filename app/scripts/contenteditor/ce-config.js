f48f215703734cff4f691ed58e9254ae41271a2b
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-03-24 15:42:58 +0530

    Issue ekstep/CE-Core-Plugins#948 fix: metadata plugin changes

--- app/scripts/contenteditor/ce-config.js
+++ app/scripts/contenteditor/ce-config.js
@@ -10,7 +10,8 @@ org.ekstep.contenteditor.config = {
         { "id": "org.ekstep.whatsnew", "ver": "1.0", "type": "plugin" },
         { "id": "org.ekstep.todo", "ver": "1.0", "type": "plugin" },        
         { "id": "org.ekstep.review", "ver": "1.0", "type": "plugin" },
-        { "id": "org.ekstep.preview", "ver": "1.1", "type": "plugin" }
+        { "id": "org.ekstep.preview", "ver": "1.1", "type": "plugin" },
+        { "id": "org.ekstep.editcontentmeta", "ver": "1.1", "type": "plugin" }
     ],
     corePluginsPackaged: true,
     dispatcher: "local",
