cedd7404212ad77a64b37e4daca2247815877737
Manjunath Davanam <manjunathd@ilimi.in>
2018-04-10 14:53:11 +0530

    #540 fix:  LOG  event issue fixes when api call is failed

--- app/scripts/contenteditor/service/telemetry-service.js
+++ app/scripts/contenteditor/service/telemetry-service.js
@@ -401,7 +401,7 @@ org.ekstep.services.telemetryService = new(org.ekstep.services.iService.extend({
             "level": data.level,
             "message": data.message,
             "params": [data],
-            "pageid": data.stage || data.pageid || ecEditor.getCurrentStage() &&  ecEditor.getCurrentStage().id
+            "pageid": data.stage || data.pageid || ecEditor.getContext('env') || "contenteditor",
         }
         EkTelemetry.log(eventData);
     },
