65a9269c45dd287b4278987d501974d618ca21b8
Akash Gupta <akash.gupta@tarento.com>
2018-05-24 11:46:05 +0530

    Issue #559 feat: Added text v1 to v2 migration plugin

---
+++ app/scripts/contenteditor/migration/textmigration-task.js
@@ -0,0 +1,40 @@
+'use strict';
+
+org.ekstep.contenteditor.migration.textmigration_task = new(Class.extend({
+    currentVersion: 1.2,
+    pluginName: 'org.ekstep.text',
+    init: function() {
+        console.log('text migration-task initialized');
+        org.ekstep.contenteditor.api.addEventListener(this.pluginName + ':migrate', this.migrateText, this);
+    },
+    migrateText: function() {
+        var instance = this;
+        var stages = org.ekstep.contenteditor.stageManager.stages;
+        _.forEach(stages, function(stage, index) {
+            _.forEach(stage.children, function(plugin){
+                if (plugin.manifest.id != instance.pluginName) return;
+                if (!plugin.attributes.version || plugin.attributes.version < instance.currentVersion) {
+                    plugin.onConfigChange("fontfamily", "NotoSansRegular");
+                    plugin.attributes.version = instance.currentVersion;
+                }
+            })
+        });
+    },
+    isOldPluginAvailable: function() {
+        var instance = this;
+        var oldPluginAvailable = false;
+        var stages = org.ekstep.contenteditor.stageManager.stages;
+        _.forEach(stages, function(stage) {
+            _.forEach(stage.children, function(plugin) {
+                if (plugin.manifest.id != instance.pluginName) return;
+                if (!plugin.attributes.version || plugin.attributes.version < instance.currentVersion) {
+                    oldPluginAvailable = true;
+                    return;
+                }
+            })
+        })
+        return oldPluginAvailable;
+    }
+}));
+
+//# sourceURL=textMigration.js
