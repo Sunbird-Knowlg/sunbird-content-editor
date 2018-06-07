9a986dc0024efbb7eae8ad782001c7a3f02e337d
Akash Gupta <akash.gupta@tarento.com>
2018-06-07 14:41:41 +0530

    Issue #559 fix: Added text migration script

--- app/scripts/contenteditor/migration/textmigration-task.js
+++ app/scripts/contenteditor/migration/textmigration-task.js
@@ -11,24 +11,21 @@ org.ekstep.contenteditor.migration.textmigration_task = new (Class.extend({
      * Assigned at the time of migrating text plugin
      * @member {String} currentVersion
      */
-    currentVersion: undefined,
-    /**
-     * Contains fonts which doesn't support default baseline config for WYSIWYG.
-     */
-    middleBaselineFonts: ["NotoSans", "NotoSansKannada", "NotoNastaliqUrdu"],
+    currentVersion: 1.2,
     /**
      * The events are registred which are used to add or remove text migrate events
      */
     init: function () {
         console.log('text migration-task initialized');
+        org.ekstep.contenteditor.api.addEventListener(this.pluginName + ':migrateAll', this.migrateAllText, this);
         org.ekstep.contenteditor.api.addEventListener(this.pluginName + ':migrate', this.migrateText, this);
     },
     /**
-     * Method to migrate the old text to new text plugin
+     * Method to migrate all the old text to new text plugin
      */
-    migrateText: function () {
-        var textPlugin = org.ekstep.pluginframework.pluginManager.pluginObjs[this.pluginName];
-        this.currentVersion = textPlugin.manifest.ver;
+    migrateAllText: function () {
+        // var textPlugin = org.ekstep.pluginframework.pluginManager.pluginObjs[this.pluginName];
+        // this.currentVersion = textPlugin.manifest.ver;
         var instance = this;
         var stages = org.ekstep.contenteditor.stageManager.stages;
         _.forEach(stages, function (stage, index) {
@@ -36,16 +33,7 @@ org.ekstep.contenteditor.migration.textmigration_task = new (Class.extend({
                 if (plugin.manifest.id != instance.pluginName) return;
                 if (!plugin.attributes.version || plugin.attributes.version < instance.currentVersion) {
                     plugin.attributes.version = instance.currentVersion;
-                    if (plugin.attributes.fontFamily == "NotoNastaliqUrdu") {
-                        plugin.attributes.offsetY = plugin.attributes.fontSize * 0.02;
-                        plugin.config.lineHeight = 1.13 * 2 * plugin.attributes.fontSize; // Defaults to 1 for other fonts and 2 for urdu
-                    } else {
-                        plugin.attributes.textBaseline = "alphabetic";
-                        plugin.attributes.offsetY = plugin.attributes.fontSize * 0.2;
-                    }
-                    if (plugin.attributes.fontFamily == "NotoSans" || plugin.attributes.fontFamily == "NotoNastaliqUrdu") {
-                        plugin.attributes.textBaseline = "middle";
-                    }
+                    TextWYSIWYG.setProperties(plugin);
                 }
             })
         });
@@ -67,7 +55,18 @@ org.ekstep.contenteditor.migration.textmigration_task = new (Class.extend({
             })
         })
         return oldPluginAvailable;
+    },
+    /**
+    * Migrate a single text
+    */
+    migrateText: function(event, plugin) {
+        if (plugin.manifest.id != this.pluginName) return;
+        if (!plugin.attributes.version || plugin.attributes.version < this.currentVersion) {
+            plugin.attributes.version = this.currentVersion;
+            TextWYSIWYG.setProperties(plugin);
+        }
     }
+
 }));
 
 //# sourceURL=textMigration.js
