ac440d863f14700523d1f619076ff326e46eda3f
Manjunath Davanam <manjunathd@ilimi.in>
2018-07-02 11:05:40 +0530

    Issue #581 fix: Removed unwanted files

--- app/scripts/contenteditor/ce-config.js
+++ app/scripts/contenteditor/ce-config.js
@@ -7,11 +7,11 @@ org.ekstep.contenteditor.config = {
     plugins: [
         { "id": "org.ekstep.developer", "ver": "1.0", "type": "plugin" },
         { "id": "org.ekstep.ceheader", "ver": "1.0", "type": "plugin" },
-        //{ "id": "org.ekstep.whatsnew", "ver": "1.0", "type": "plugin" },
-        //{ "id": "org.ekstep.todo", "ver": "1.1", "type": "plugin" },        
-        //{ "id": "org.ekstep.review", "ver": "1.0", "type": "plugin" },
-        //{ "id": "org.ekstep.preview", "ver": "1.1", "type": "plugin" },
-        //{ "id": "org.ekstep.editcontentmeta", "ver": "1.1", "type": "plugin" },
+        { "id": "org.ekstep.whatsnew", "ver": "1.0", "type": "plugin" },
+        { "id": "org.ekstep.todo", "ver": "1.1", "type": "plugin" },
+        { "id": "org.ekstep.review", "ver": "1.0", "type": "plugin" },
+        { "id": "org.ekstep.preview", "ver": "1.1", "type": "plugin" },
+        { "id": "org.ekstep.editcontentmeta", "ver": "1.1", "type": "plugin" },
         { "id": "org.ekstep.quiz", "ver": "1.0", "type": "plugin" }
     ],
     corePluginsPackaged: true,
