f48606d4d45a2b547bbf548281c8053da2900539
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-07-27 17:10:59 +0530

    Issue #000 fix: activity browser changes for `Local content index within Sunbird`

--- webpack.plugin.config.js
+++ webpack.plugin.config.js
@@ -26,7 +26,7 @@ var corePlugins = [
     "org.ekstep.stageconfig-1.0",
     "org.ekstep.telemetry-1.0",
     "org.ekstep.preview-1.1",
-    "org.ekstep.activitybrowser-1.2",
+    "org.ekstep.activitybrowser-1.3",
     "org.ekstep.collaborator-1.1",
     "org.ekstep.download-1.0",
     "org.ekstep.unsupported-1.0",
