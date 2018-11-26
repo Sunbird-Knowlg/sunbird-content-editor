4ffddfdae3e24a1f7ac80ca3a38d50b32cd237ad
Rajesh Gupta <rajesh.gupta@compassites.net>
2018-11-26 17:44:38 +0530

    Issue #SB-8855 fix:Implementaion of service to call downloadtoc api to get textbook toc csv file

--- app/scripts/framework/service/content-service.js
+++ app/scripts/framework/service/content-service.js
@@ -211,7 +211,7 @@ org.ekstep.services.contentService = new (org.ekstep.services.iService.extend({
      */
 	downloadTableContent: function (contentId, fileName, callback) {
 		var data = { 'request': { 'content_identifiers': [contentId], 'file_name': fileName } }
-		this.postFromService(this.serviceURL() + this.getConfig('contentBundleUrl', '/v3/bundle'), data, this.requestHeaders, callback)
+		this.getFromService(this.serviceURL() + this.getConfig('textbook', '/v1/download'), contentId, this.requestHeaders, callback)
 	},
 	/**
      *
