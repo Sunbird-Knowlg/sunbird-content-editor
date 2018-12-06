1663c45ac1a627d7aec9c8d9ec08bb9d53e37afa
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-12-06 15:34:15 +0530

    Issue #SB-9112 fix: textbook service updated

--- app/scripts/framework/service/textbook-service.js
+++ app/scripts/framework/service/textbook-service.js
@@ -9,11 +9,11 @@ org.ekstep.services.textbookService = new(org.ekstep.services.iService.extend({
         return this.getBaseURL() + this.getAPISlug() + this.getConfig('textbookEndPoint', '/textbook')
     },
     uploadFile: function(contentId, fileUrl, callback) {
-        this.postFromService(this.serviceURL() + this.getConfig('uploadContentURL', '/v1/toc/upload/') + contentId + '?fileUrl=' + fileUrl, {}, config, callback)
+        this.postFromService(this.serviceURL() + this.getConfig('uploadTocURL', '/v1/toc/upload/') + contentId + '?fileUrl=' + fileUrl, {}, config, callback)
     },
     downloadFile: function(contentId, callback) {
         var headersObj = _.cloneDeep(this.requestHeaders)
         headersObj.headers['content-type'] = "application/x-download";
-        this.getFromService(this.serviceURL() + this.getConfig('donwnloadTextbookUrl', '/v1/toc/download/') + contentId, headersObj, callback)
+        this.getFromService(this.serviceURL() + this.getConfig('donwnloadTocUrl', '/v1/toc/download/') + contentId, headersObj, callback)
     }
 }))()
