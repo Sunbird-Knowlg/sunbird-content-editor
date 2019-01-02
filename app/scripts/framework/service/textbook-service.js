1dc8b7220e3808b09b9dbf3de84a87d2d9f4ef9f
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-01-02 10:25:45 +0530

    Issue #SB-0000 fix: indentation issues fixed

--- app/scripts/framework/service/textbook-service.js
+++ app/scripts/framework/service/textbook-service.js
@@ -4,16 +4,16 @@
  * @class org.ekstep.services.textbookService
  * @author Kartheek Palla <kartheekp@ilimi.in>
  */
-org.ekstep.services.textbookService = new(org.ekstep.services.iService.extend({
-    serviceURL: function() {
-        return this.getBaseURL() + this.getAPISlug() + this.getConfig('textbookEndPoint', '/textbook')
-    },
-    uploadFile: function(contentId, fileUrl, callback) {
-        this.postFromService(this.serviceURL() + this.getConfig('uploadTocURL', '/v1/toc/upload/') + contentId + '?fileUrl=' + fileUrl, {}, this.requestHeaders, callback)
-    },
-    downloadFile: function(contentId, callback) {
-        var headersObj = _.cloneDeep(this.requestHeaders)
-        headersObj.headers['content-type'] = "application/x-download";
-        this.getFromService(this.serviceURL() + this.getConfig('donwnloadTocUrl', '/v1/toc/download/') + contentId, headersObj, callback)
-    }
+org.ekstep.services.textbookService = new (org.ekstep.services.iService.extend({
+	serviceURL: function () {
+		return this.getBaseURL() + this.getAPISlug() + this.getConfig('textbookEndPoint', '/textbook')
+	},
+	uploadFile: function (contentId, fileUrl, callback) {
+		this.postFromService(this.serviceURL() + this.getConfig('uploadTocURL', '/v1/toc/upload/') + contentId + '?fileUrl=' + fileUrl, {}, this.requestHeaders, callback)
+	},
+	downloadFile: function (contentId, callback) {
+		var headersObj = _.cloneDeep(this.requestHeaders)
+		headersObj.headers['content-type'] = 'application/x-download'
+		this.getFromService(this.serviceURL() + this.getConfig('donwnloadTocUrl', '/v1/toc/download/') + contentId, headersObj, callback)
+	}
 }))()
