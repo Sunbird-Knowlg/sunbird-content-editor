ab0281628acde5400158429a733133e260f2d091
Kartheek Palla <palla.kartheekreddy@gmail.com>
2019-03-18 12:31:48 +0530

    Issue #SB-11001 fix: codacy fixes

--- app/scripts/contenteditor/manager/stage-manager.js
+++ app/scripts/contenteditor/manager/stage-manager.js
@@ -272,8 +272,8 @@ org.ekstep.contenteditor.stageManager = new (Class.extend({
 			content.theme['migration-media'].media = _.values(org.ekstep.contenteditor.mediaManager.migratedMediaMap)
 		}
 		content.theme.manifest.media = _.uniqBy(_.concat(content.theme.manifest.media, _.values(mediaMap)), 'id')
-		if(!_.isEmpty(org.ekstep.contenteditor.migration.patch)){
-			content.theme['patch'] = org.ekstep.contenteditor.migration.patch.toString();
+		if (!_.isEmpty(org.ekstep.contenteditor.migration.patch)) {
+			content.theme['patch'] = org.ekstep.contenteditor.migration.patch.toString()
 		}
 		return _.cloneDeep(content)
 	},
