11c81d6b19479b1a3bf19b9f37f8b030ceeab0b7
Sivashanmugam Kannan <sivashanmugam.kannan@funtoot.com>
2019-07-12 19:00:00 +0530

    Issue #SC-1050 fix : fixing overriding of global cdata

--- app/scripts/framework/service/iservice.js
+++ app/scripts/framework/service/iservice.js
@@ -7,8 +7,14 @@ org.ekstep.services.iService = Class.extend({
 	requestHeaders: {
 		'headers': {
 			'content-type': 'application/json',
-			'user-id': 'content-editor'
+			'user-id': 'content-editor',
+			'X-Channel-ID': 'b00bc992ef25f1a9a8d63291e20efc8d',
+			'x-authenticated-userid': '874ed8a5-782e-4f6c-8f36-e0288455901e',
+			'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkMTc1MDIwNDdlODc0ODZjOTM0ZDQ1ODdlYTQ4MmM3MyJ9.7LWocwCn5rrCScFQYOne8_Op2EOo-xTCK5JCFarHKSs',
+			// only x-auth need to be changed, others waste
+			'x-authenticated-user-token': 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ1WXhXdE4tZzRfMld5MG5PS1ZoaE5hU0gtM2lSSjdXU25ibFlwVVU0TFRrIn0.eyJqdGkiOiIwZDk4ZTI3ZC0zOGNhLTQ0ODItOTJmZi0xMjQwMDhmMmM5M2YiLCJleHAiOjE1NjIzMTMyNDIsIm5iZiI6MCwiaWF0IjoxNTYyMzEyOTQyLCJpc3MiOiJodHRwczovL3N0YWdpbmcubnRwLm5ldC5pbi9hdXRoL3JlYWxtcy9zdW5iaXJkIiwiYXVkIjoiYWRtaW4tY2xpIiwic3ViIjoiZjo5MzFhOWRjOS00NTk0LTQ4MzktYWExNi1jZjBjYWMwOTYzODE6NjQ0ZGE0OWMtN2JlZi00OWQ2LThlMjQtZTc0MWQxNDhkNjUyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWRtaW4tY2xpIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZTZhM2M0MTctNjE5Mi00YjY1LWI2ZjktYjdlZWUzMTg0M2EyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7fSwibmFtZSI6IjEuMTBDcmVhdG9yIFVzZXIiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjcmVhdG9yMS4xMCIsImdpdmVuX25hbWUiOiIxLjEwQ3JlYXRvciIsImZhbWlseV9uYW1lIjoiVXNlciIsImVtYWlsIjoiY3JlYXRvcnVzZXIxLjEwQHlhaG9vLmNvbSJ9.Kr1cPQKxfGJn5BU4s5G6avFGKJovQftNER97ZAJXgJJoS03K0AgAX1EGTVKzBTouJWJSIvo3AF-TSdi1O_9D6Ev6c_rZuZQmXAB6rjpgF2tzFM40KVEVs9H532YJWY45M86spR-DM1vPfsFJfSnpWf3HHms_io7nFccpu4wUNk0-DDvAwaE-2yK7CqMx-a6j4bmhtpn6rUQUeoekCin2Ss1FhBbNgpxEZaCc8fp0j8nsBCIZt--mcrKjiUPFWvXvIFB0svpKvlATmYtw0OxRyWNL6aeACCFK_7E2hbOn2Gl0ZpKVNRp_pMlDjm6wItxW6XVhwkMNMt8-i_qfl73W5w' 
 		}
+
 	},
 	getBaseURL: function () {
 		return org.ekstep.services.config.baseURL
@@ -34,20 +40,16 @@ org.ekstep.services.iService = Class.extend({
 		ajaxSettings.beforeSend = function (xhrObject, settings) {
 			requestTimestamp = (new Date()).getTime()
 		}
-		ajaxSettings.success = function (res) {
-			res.responseTime = (new Date()).getTime() - requestTimestamp
+		ajaxSettings.success = function (data, res, xhr) {
+			data.responseTime = (new Date()).getTime() - requestTimestamp
 			var request = ajaxSettings.type === 'POST' ? ajaxSettings.data : {}
-			instance._dispatchTelemetry({ url: ajaxSettings.url, method: ajaxSettings.type, request: request, res: res })
-			res = { data: res }
+			instance._dispatchTelemetry({ url: ajaxSettings.url, method: ajaxSettings.type, request: request, res: data })
+			res = { data: data }
 			cb(null, res)
 		}
 		ajaxSettings.error = function (err) {
 			err.responseTime = (new Date()).getTime() - requestTimestamp
-			if(err && err.status === 401 && err.statusText === "Unauthorized") {
-				ecEditor.dispatchEvent("org.ekstep.contenteditor:Unauthorized");
-			} else {
-				cb(err, null);
-			}
+			cb(err, null)
 			var request = ajaxSettings.type === 'POST' ? ajaxSettings.data : {}
 			instance._dispatchTelemetry({ url: ajaxSettings.url, method: ajaxSettings.type, request: request, res: err })
 		}
